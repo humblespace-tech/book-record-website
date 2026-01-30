fb7a382ac81e47cbb49effcd45838ee39dabe137
humblespace <cryptomumbles@gmail.com>
2026-01-30 23:44:18 +0800

    Fix import path for Home.module.css

--- pages/pages/index.js
+++ pages/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import styles from '../../styles/styles/Home.module.css'
 export default function Home() {
     return (
           <div className={styles.container}>
